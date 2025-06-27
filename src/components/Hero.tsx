cece262578a62501b767fc3a50ab793e8a003c43
prai-today <jay@prai.today>
2025-06-27 17:02:52 +0900

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -25,7 +25,7 @@ export function Hero() {
 
       <div className="absolute z-100 top-16 sm:top-6 right-6">
         
-      <img src="/bolt.png" alt="" className="w-16 sm:w-20" />
+      <img src="/bolt.png" alt="" className="w-16 sm:w-20 animate-spin" />
       </div>
       {/* Enhanced background decorative elements */}
       <div className="absolute inset-0 overflow-hidden">
