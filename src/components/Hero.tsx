1daa40ddb401a02caa997d44393f8231229d1be9
prai-today <jay@prai.today>
2025-06-27 16:56:09 +0900

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -24,6 +24,7 @@ export function Hero() {
     <div className="relative min-h-screen bg-gradient-to-br from-indigo-50 via-white to-purple-50 flex items-center overflow-hidden">
 
       <div className="absolute z-100 top-0 right-0">
+        <p>Hellow</p>
       <img src="/bolt.jpg" alt="" />
       </div>
       {/* Enhanced background decorative elements */}
